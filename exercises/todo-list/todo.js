652634c34c87d785632cea64298e8bf02342b7a6
Evan DENIS <evan@octopusbiosafety.com>
2023-07-19 11:59:25 +0200

    modified:   exercises/todo-list/todo-list.html 	modified:   exercises/todo-list/todo.css 	modified:   exercises/todo-list/todo.js

--- exercises/todo-list/todo.js
+++ exercises/todo-list/todo.js
@@ -1,22 +1,42 @@
 const toDoList = JSON.parse(localStorage.getItem('toDoList')) || [];
 
-console.log(toDoList)
+displayToDoList()
 
 function addToDo() {
   const taskInput = document.querySelector('.newTaskBar')
-  toDoList.push(taskInput.value)
+  const dateInput = document.querySelector('.newTaskDate')
+  toDoList.push({
+      name: taskInput.value,
+      date: dateInput.value
+    })
   taskInput.value = ''
-  localStorage.setItem('toDoList', JSON.stringify(toDoList))
-  displayToDoList()
+  dateInput.value = ''
+  displayToDoList(dateInput.value)
 }
 
 function displayToDoList() {
+  trierListeParDate(toDoList)
   let toDoCode = ''
   for (i=0; i<toDoList.length; i++) {
     toDoCode += `<div class="taskAndDelete">
-                  <div class="taskToDo">${toDoList[i]}</div>
-                  <button class="deleteButton">Delete</button>
+                  <div class="taskToDo"><p class="taskText">${toDoList[i].name}</p> <p class="taskText">${toDoList[i].date}</p></div>
+                  <button class="deleteButton" onclick="deleteTask(${i})">Delete</button>
                 </div>`
   }
+  if(toDoCode === '') {toDoCode ='<p class="noTaskText">No task yet</p>'}
   document.querySelector('.taskList').innerHTML = toDoCode
+  localStorage.setItem('toDoList', JSON.stringify(toDoList))
+}
+
+function deleteTask(thisId) {
+  toDoList.splice(thisId, 1)
+  displayToDoList()
+}
+
+function trierListeParDate(listeObjets) {
+  return listeObjets.sort(function(a, b) {
+    var dateA = new Date(a.date);
+    var dateB = new Date(b.date);
+    return dateA - dateB;
+  });
 }
